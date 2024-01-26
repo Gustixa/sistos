3ebeafbf5744fd7dabe69919d42d1dce8fbcd003
Samuel Argueta <arg211024@uvg.edu.gt>
2024-01-26 14:53:55 -0600

    Modificacion

---
+++ p1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+  int i;
+  for (i = 0; i < 4; ++i) {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+      // Proceso hijo, codigo
+      printf("Proces hijo %d, PID: %d, PPID: %d\n", i + 1, getpid(), getppid());
+      break;
+    } else if (pid < 0) {
+      // Manejo de posibles errores al crear el proceso hijo
+      fprintf(stderr, "Error al crear el proceso hijo %d\n", i+1);
+      return 1;
+    }
+  }
+  // Proceso padre
+  if (i == 4) {
+    for (i = 0; i < 4; ++i) {
+      wait(NULL);
+    }
+  }
+  return 0;
+}
